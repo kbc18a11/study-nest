4f3e6aafb3574eaa8609bd06e49f838907f26daf
kbc18a11 <wada121400@gmail.com>
2021-04-25 16:17:04 +0900

    次はhttps://docs.nestjs.com/middleware#middleware-consumer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+  }
+}
