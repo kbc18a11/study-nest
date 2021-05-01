fcadcfca07bc4024891f7939d7636942dfe68d2c
kbc18a11 <wada121400@gmail.com>
2021-05-01 21:40:45 +0900

    https://docs.nestjs.com/interceptors

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Req, Post, Param, Body, ParseIntPipe, UsePipes } from '@nestjs/common';
+import { Controller, Get, Req, Post, Param, Body, ParseIntPipe, UsePipes, UseGuards } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Request } from 'express';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+import { RolesGuard } from '../guard/role.guard';
 import { ValidationPipe } from "../validation/JoiValidationPipe.pipe";
 
 @Controller('cats')
@@ -15,6 +16,7 @@ export class CatsController {
     }
 
     @Post()
+    @UseGuards(RolesGuard)
     async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
         this.catsService.create(createCatDto);
     }
